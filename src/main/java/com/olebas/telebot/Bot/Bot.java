ddd5ce057531ade2f0aced4af0618e5f77b10628
olebas13 <o.nevoyt13@gmail.com>
2019-03-06 18:45:16 +0200

    commit for heroku

--- src/main/java/com/olebas/telebot/Bot/Bot.java
+++ src/main/java/com/olebas/telebot/Bot/Bot.java
@@ -7,43 +7,108 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class Bot extends TelegramLongPollingBot {
 
     private long chat_id;
+    String lastMessage = "";
+    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
     private static ReadFileData data = new ReadFileData();
 
     public void onUpdateReceived(Update update) {
         update.getUpdateId();
         SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
         chat_id = update.getMessage().getChatId();
-        sendMessage.setText(input(update.getMessage().getText()));
+
+        String text = update.getMessage().getText();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
 
         try {
+            sendMessage.setText(getMessage(text));
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
 
-    public String input(String msg) {
-        if (msg.contains("Hi") || msg.contains("Hello") || msg.contains("Привет")) {
-            return "Привет, братан!";
+    private String getMessage(String msg) {
+        ArrayList<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        if (msg.contains("/start")) {
+            return "Привет!";
         }
-        if (msg.contains("Информация о книге")) {
-            return getInfoBook();
-        }
+
         if (msg.contains("/person")) {
             msg = msg.replace("/person ", "");
             return getInfoPerson(msg);
         }
-        return "He понял!";
+
+        if (msg.equals("Привет") || msg.equals("Меню")) {
+            keyboard.clear();
+            keyboardFirstRow.clear();
+            keyboardFirstRow.add("Популярное");
+            keyboardFirstRow.add("Новости\uD83E\uDDD0");
+            keyboardSecondRow.add("Полезная информация!");
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return "Выбрать...";
+        }
+
+        if (msg.equals("Полезная информация!")) {
+            keyboard.clear();
+            keyboardFirstRow.clear();
+            keyboardFirstRow.add("Информация о книге");
+            keyboardFirstRow.add("/person bebosehun_");
+            keyboardFirstRow.add("Меню");
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return "Выбрать...";
+        }
+
+        if (msg.equals("Популярное")) {
+            keyboard.clear();
+            keyboardFirstRow.clear();
+            keyboardFirstRow.add("Стихи");
+            keyboardFirstRow.add("Книги");
+            keyboardFirstRow.add("Меню");
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return "Выбрать...";
+        }
+
+        return "Не понял!";
     }
+
+//    public String input(String msg) {
+//        if (msg.contains("Hi") || msg.contains("Hello") || msg.contains("Привет")) {
+//            return "Привет, братан!";
+//        }
+//        if (msg.contains("Информация о книге")) {
+//            return getInfoBook();
+//        }
+//        if (msg.contains("/person")) {
+//            msg = msg.replace("/person ", "");
+//            return getInfoPerson(msg);
+//        }
+//        return "He понял!";
+//    }
 
     public String getInfoBook(){
         Book book = new Book();
